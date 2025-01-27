ccdb9629e455eb00082d3f9730deaa0bcee06cae
MasonBurzy <mason.burzynski@bsu.edu>
2025-01-27 14:07:57 +0000

    added hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+///   Hello World Program   ///
+
+
+#include <iostream>
+
+
+// start main
+
+int main(){
+	std::string userName
+	
+	std::cout << "Hi, please enter in your name. ";
+	std::cin >> userName;
+
+	std::cout << "Hello " << userName << ". Nice to meet you. " << std::end1;
+
+	return 0;
+
+} // end main
