475151dff82249f483d2b591c6ea1a8733fff852
MasonBurzy <mason.burzynski@bsu.edu>
2025-01-31 14:47:25 +0000

    modified horse.cpp

--- horse.cpp
+++ horse.cpp
@@ -5,27 +5,38 @@
 std::random_device rd;
 std::uniform_int_distribution<int> dist(0, 1);
 
-int coin = dist(rd);
 
-void advance(int horseNum, int* horses);
-void printLane(int horseNum, int* horses);
-bool isWinner(int horseNum, int* horses);
+int horseNum = 5;
+int trackLength = 10;
+void advance(int numHorse, int* horses);
+void printLane(int numHorse, int* horses);
+bool isWinner(int numHorse, int* horses);
 
 
 int main(){
-	int horseNum = 5;
-	int trackLength = 10;
 	int horses [] = {0, 0, 0, 0, 0};
 	bool keepGoing = true;
 
 	while (keepGoing){
-	
-	}
+		for (int hn = 0; hn < numHorse; hn++){
+			advance(hn, horses)
+			printLane(hn, horses);
+			if (isWinner(hn, horses)){
+				keepGoing = false;
+			} // end if
+		} // end for
 
-}
+		std::cout << "Press enter for another turn";
+		std::cin.ignore();
 
+	} // end while
 
+	return 0;
+
+/*
 advance(){
 
 
  }
+
+ */
