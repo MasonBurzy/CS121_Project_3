ea32faf6373acf9d597939bf9a544c9320572a4b
MasonBurzy <mason.burzynski@bsu.edu>
2025-02-01 18:54:19 +0000

    modified horse.cpp

--- horse.cpp
+++ horse.cpp
@@ -5,23 +5,26 @@
 std::random_device rd;
 std::uniform_int_distribution<int> dist(0, 1);
 
-
-int horseNum = 5;
-int trackLength = 10;
-void advance(int numHorse, int* horses);
-void printLane(int numHorse, int* horses);
-bool isWinner(int numHorse, int* horses);
+int coin = dist(rd);
 
 
-int main(){
+int NumHorses = 5;
+int TrackLength = 10;
+
+void advance(int HorseNum, int* horses);
+void printLane(int HorseNum, int* horses);
+bool isWinner(int HorseNum, int* horses);
+
+
+int main() {
 	int horses [] = {0, 0, 0, 0, 0};
 	bool keepGoing = true;
 
-	while (keepGoing){
-		for (int hn = 0; hn < numHorse; hn++){
-			advance(hn, horses)
+	while (keepGoing) {
+		for (int hn = 0; hn < NumHorses; hn++) {
+			advance(hn, horses);
 			printLane(hn, horses);
-			if (isWinner(hn, horses)){
+			if (isWinner(hn, horses)) {
 				keepGoing = false;
 			} // end if
 		} // end for
@@ -33,10 +36,37 @@ int main(){
 
 	return 0;
 
-/*
-advance(){
+} // end main
 
-
+void advance(int HorseNum, int horses[]) {
+	 horses[HorseNum] += coin;
+	
  }
 
- */
+void printLane(int HorseNum, int horses[]){
+	for (int hn = 0; hn < TrackLength; hn++) {
+		if(hn == horses[HorseNum]) {
+			std::cout << " {hn}" << std::endl;
+		} // end if 
+		
+		else {
+			std::cout << "." << std::endl;
+		} // end else
+
+	} // end for
+
+} // end printLane
+
+bool isWinner(int HorseNum, int horses[]) {
+bool result = false;
+	if (horses[HorseNum] == TrackLength) {
+			result = true;
+			std::cout << "Horse {hn} won!" << std::endl;
+	} // end if
+	return result;
+
+} // end isWinner
+
+
+
+
